0842cf4ad656cfb09d55fa5ecbe63133c6024fc9
Jalal Zainaddin <jalalzainaddin@gmail.com>
2024-02-07 19:23:33 +0300

    Day 2

--- Day 2/DOM/script.js
+++ Day 2/DOM/script.js
@@ -1,7 +1,30 @@
 // Add functionality
-
+document.getElementById("add").onclick = ()=>{
+    value = parseInt(document.getElementById("counter").innerHTML);
+    value++;
+    document.getElementById("counter").innerHTML = value
+}
 // Subtract functionality
-
+document.getElementById("sub").onclick = ()=>{
+    value = parseInt(document.getElementById("counter").innerHTML);
+    value >0 ? value--:value = 0;
+    document.getElementById("counter").innerHTML = value
+}
 // Change functionality
-
+document.getElementById("change").onclick = ()=>{
+    value = window.prompt("Enter new image URL","")
+}
 // Form submit functionality
+document.getElementById("form").addEventListener("submit",()=>
+{
+    name = document.getElementById("name").value
+    level = document.getElementById("level").value
+    phone = document.getElementById("phone").value
+    id = document.getElementById("id").value
+    alert(`
+    ${name}
+    ${level}
+    ${phone}
+    ${id}
+    `)
+})
--- Day 2/JS/js103.js
+++ Day 2/JS/js103.js
@@ -1,22 +1,22 @@
-// function add(x,y){
-//     return x+ y
-// }
-// let substract = function(x,y){
-//     return Math.abs(x-y)
-// }
+function add(x,y){
+    return x+ y
+}
+let substract = function(x,y){
+    return Math.abs(x-y)
+}
 
-// let multiply = (x,y) => x*y;
+let multiply = (x,y) => x*y;
 
-// let input = "add"
-// let x = 2;
-// let y = 2;
+let input = "add"
+let x = 2;
+let y = 2;
 
 
-// let z =
-// input === "add"? console.log(add(x,y))
-// :
-//     input ==="mult"?
-//         console.log(multiply(x,y)): console.log(substract(x,y))
+let z =
+input === "add"? console.log(add(x,y))
+:
+    input ==="mult"?
+        console.log(multiply(x,y)): console.log(substract(x,y))
 
 //// task2
 
